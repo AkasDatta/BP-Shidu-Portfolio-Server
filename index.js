3a9529c05a56e576fcdab0d9c046190a2f6f7376
Akas_Datta <012akasdatta@gmail.com>
2024-12-15 22:50:42 +0600

    removed space

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.get('/', (req, res) => (
     res.send('Boss is sitting')
 ))
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqxu2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
